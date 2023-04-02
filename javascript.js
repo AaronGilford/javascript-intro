f166a078e224f69d73f160f1f5870bd30d303ab4
AaronGilford <127493995+AaronGilford@users.noreply.github.com>
2023-04-02 10:25:13 +0100

    Comment different types of basic javascript

---
+++ javascript.js
@@ -0,0 +1,59 @@
+// let message;
+
+// message = 'Hello!' //store the string 'Hello' in the variable named message
+
+// alert(message); //shows the variable content
+
+//Can be done in one line with let message = 'Hello!';
+
+let user = 'John', age = '25';
+
+//multiple variables in one line
+//better readability to separate by line
+
+//let message;
+
+//message = 'Hello';
+
+//message = 'World'; //overrides previous value
+
+//alert(message);
+
+//let hello = "Hello World!";
+
+//let message = hello;
+
+// copy hello world from hello into message
+
+//alert(hello);
+//alert(message);
+
+//declaring a variable twice creates an error
+
+// variable names must only contain letter, numbers and the symbols '$' and '_' and the first character must not be a digit
+
+// camelCase is used for multiple words somethingLikeThis
+
+//const myBirthDate = '18.04.1998';
+
+// For values that will remain constant, cant be overwritten!
+
+//const COLOR_RED = '#F00';
+
+// Widespread practice to use constants as alliases for difficult to remember values, with capital letters. Only if known prior.
+
+//let name = "John";
+//let admin =  name;
+
+//alert(admin);
+
+//let ourPlanet = "Earth";
+
+
+//let currentVisitor = "John";
+
+let x = 5;
+let y = 2;
+let z = x % y;
+
+alert(z);
